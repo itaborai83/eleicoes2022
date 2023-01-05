f4b364bf16d98298052cc09de8b09eaf1a1b0315
itaborai83 <itaborai83@gmail.com>
2023-01-05 00:12:04 -0300

    deleting old crap

--- eleicoes2022/config.py
+++
@@ -1,6 +0,0 @@
-import os
-import sqlalchemy
-POSTGRES_DSN            = os.environ['POSTGRES_DSN']
-POSTGRES_SQLALCHEMY_DSN = os.environ['POSTGRES_SQLALCHEMY_DSN']
-POSTGRES_ENGINE         = sqlalchemy.create_engine(POSTGRES_SQLALCHEMY_DSN)
-MAPS_APIKEY             = os.environ['MAPS_APIKEY']
--- eleicoes2022/ingestion/config.py
+++
@@ -1,7 +0,0 @@
-CSV_ENCODING            = "latin1"
-IMPORT_DATE_MASK        = "%d/%m/%Y"
-IMPORT_TIME_MASK        = "%H:%M:%S"
-IMPORT_DATETIME_MASK    = IMPORT_DATE_MASK + " " + IMPORT_TIME_MASK
-DATE_MASK               = "%Y-%m-%d"
-TIME_MASK               = "%H:%M:%S"
-DATETIME_MASK           = DATE_MASK + " " + TIME_MASK
